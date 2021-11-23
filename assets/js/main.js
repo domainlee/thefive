8c74400bf152f0b26bcaf56a63551ac70a5ed112
Admin <admin@MacBook-Pro-cua-le.local>
2021-11-23 08:26:25 +0700

    first app

---
+++ assets/js/main.js
@@ -0,0 +1,142 @@
+;(function () {
+    'use strict';
+
+    var nav = function () {
+        var button_nav = $('.toggle-menu');
+        button_nav.click(function (e) {
+            $('body').toggleClass('nav-open-js');
+        });
+
+        var toggle_search = $('.toggle-search');
+        toggle_search.click(function (e) {
+            $('body').toggleClass('search-open-js');
+        });
+
+        $("html").click(function(e) {
+            if ($(e.target).closest('.search-mobile').length == 0)
+                $('body').removeClass('search-open-js');
+        });
+
+        $('.nav__mobile > ul > li a').each(function(){
+            var t = $(this);
+            var checkElement = t.next();
+            if(checkElement.is('ul')) {
+                t.after('<span class="more">+</span>');
+                t.next().click(function(e){
+                    e.preventDefault();
+                    if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+                        checkElement.slideUp('normal');
+                    }
+                    if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+                        checkElement.slideDown('normal');
+                    }
+                    $(this).toggleClass('active');
+                });
+            }
+
+        });
+
+        // $('.nav__mobile > ul').append('<a class="button__close"></a>');
+        //
+        // $('.button__close').click(function () {
+        //     $('body').removeClass('nav-open-js');
+        // });
+    }
+
+    var lazy = function () {
+        $('.lazy').Lazy({
+            effect: "fadeIn",
+            effectTime: 500
+        });
+    };
+
+    var owlCarousel = function() {
+
+        // Home page slider //
+        $('.home-three-feature__js').owlCarousel({
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+            items: 1,
+            margin: 0,
+            stagePadding: 0,
+            smartSpeed: 350,
+            slideSpeed: 350,
+            loop: true,
+            autoplay:true,
+            lazyLoad: true,
+            dots: false,
+            nav: true,
+            navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        });
+
+        // Home page section highlights two post //
+        $('.highlights-post__list--js').owlCarousel({
+            loop: true,
+            margin: 30,
+            dots: false,
+            nav: true,
+            autoplay: true,
+            navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 2
+                }
+            },
+
+        });
+    };
+
+    var masonry = function () {
+        $('.grid-js').masonry({
+            itemSelector: '.post-type-two__item',
+            columnWidth: '.grid-size',
+        });
+    };
+
+    var search = function () {
+        $('.head__button-search').click(function(){
+            $('body').toggleClass('search-js-open');
+        });
+    };
+
+    var sidebarScroll = function()
+    {
+        var tmpWindow = $(window),
+            wrapper = $('.main').height(),
+            header = $('.head').height(),
+            sidebar = $('.sidebar');
+
+        if (sidebar.length > 0) {
+            var offsetTop = sidebar.offset().top,
+                offsetBottom;
+
+            tmpWindow.scroll(function(){
+
+                offsetBottom = (wrapper + header) - sidebar.height();
+
+                if (tmpWindow.scrollTop() < offsetTop) {
+                    sidebar.removeClass('fixed bottom');
+                } else if (tmpWindow.scrollTop() > offsetBottom) {
+                    sidebar.removeClass('fixed').addClass('bottom');
+                } else {
+                    sidebar.removeClass('bottom').addClass('fixed');
+                }
+            });
+        }
+    }
+
+    $(document).ready(function() {
+        nav();
+        lazy();
+        owlCarousel();
+        // masonry();
+        search();
+        // sidebarScroll();
+    });
+}());
