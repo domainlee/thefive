b4b8e62f15080cf9e20d2e005a588888ef2c223e
Admin <admin@MacBook-Pro-cua-le.local>
2022-04-10 01:10:46 +0700

    Updated style

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,25 +63,16 @@
             navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
         });
 
-        // Home page section highlights two post //
-        $('.highlights-post__list--js').owlCarousel({
+        // SINGLE PAGE - Gallery //
+        $('.single-post__gallery--js').owlCarousel({
             loop: true,
-            margin: 30,
-            dots: false,
+            margin: 0,
+            dots: true,
             nav: true,
+            lazyLoad: true,
             autoplay: true,
+            items: 1,
             navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 2
-                },
-                1000: {
-                    items: 2
-                }
-            },
         });
     };
 
